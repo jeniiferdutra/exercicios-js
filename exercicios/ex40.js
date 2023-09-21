83879799e831cc305232047a5399e95195c49cec
Jenifer <jeniferdutra2000@icloud.com>
2023-09-21 00:15:45 -0300

    continue

---
+++ exercicios/ex40.js
@@ -0,0 +1,9 @@
+// O que fazer para total retornar 500?
+
+const numero = 50 // Definir como const
+
+for(let numero = 0; numero < 10; numero++) { // Definir como let
+    console.log(numero)
+}
+const total = 10 * numero
+console.log(total)
